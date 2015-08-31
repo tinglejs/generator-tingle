ff53fcd720f0bc74c704775b4c33cdbd7aadf79c
曹柯 <caoke.ck@alibaba-inc.com>
2015-08-31 17:58:09 +0800

    add tingle-context

--- app/templates/demo/index.js
+++ app/templates/demo/index.js
@@ -5,6 +5,7 @@
  * Copyright 2014-2015, Tingle Team, Alinw.
  * All rights reserved.
  */
+require('tingle-context');
 window.FaskClick && FastClick.attach(document.body);
 var Demo = require('./<%= ComponentName %>Demo');
 React.render(<Demo/>, document.getElementById('TingleDemo'));
